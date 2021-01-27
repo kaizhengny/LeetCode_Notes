807cfca9d7a60e4813dbc6667647f02cf6be88af
Kai Zheng <52578774+kaizhengny@users.noreply.github.com>
2021-01-27 11:14:29 -0500

    Create 1680. Concatenation of Consecutive Binary Numbers.py

---
+++ Bitwise/1680. Concatenation of Consecutive Binary Numbers.py
@@ -0,0 +1,18 @@
+1680. Concatenation of Consecutive Binary Numbers
+Medium
+
+Given an integer n, return the decimal value of the binary string formed by concatenating 
+the binary representations of 1 to n in order, modulo 109 + 7.
+
+
+Example 1:
+
+Input: n = 1
+Output: 1
+Explanation: "1" in binary corresponds to the decimal value 1. 
+Example 2:
+
+Input: n = 3
+Output: 27
+Explanation: In binary, 1, 2, and 3 corresponds to "1", "10", and "11".
+After concatenating them, we have "11011", which corresponds to the decimal value 27.
